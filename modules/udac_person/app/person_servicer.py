58e88366394529cf82c40d0ca9dd151da467de20
Uwe Sülter <uwe.suelter@jtelekom.de>
2025-03-26 18:04:25 +0100

    adding gprc

---
+++ modules/udac_person/app/person_servicer.py
@@ -0,0 +1,25 @@
+import grpc
+import person_pb2
+import person_pb2_grpc
+from app.udac_person.services import PersonService
+
+class  PersonServiceServcier(person_pb2_grpc.PersonServiceServicer):
+    def Get(self, request, context):
+ # Retrieve all persons from the database using the service
+        persons = PersonService.retrieve_all()
+        
+        # Create a PersonList message
+        person_list = person_pb2.PersonList()
+        
+        # Translate each Person model instance into a gRPC Person message
+        for person in persons:
+            grpc_person = person_pb2.Person(
+                id=str(person.id),
+                first_name=person.first_name,
+                last_name=person.last_name,
+                company_name=person.company_name
+            )
+            # Add the gRPC Person message to the PersonList
+            person_list.person.append(grpc_person)
+        
+        return person_list
--- modules/udac_person/wsgi.py
+++ modules/udac_person/wsgi.py
@@ -1,7 +1,22 @@
 import os
-
+import grpc
+import person_pb2_grpc
+from app.person_servicer import PersonServiceServicer
+from concurrent import futures
 from app import create_app
 
+def serve_grpc():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    person_pb2_grpc.add_PersonServiceServicer_to_server(PersonServiceServicer(), server)
+    server.add_insecure_port('[::]:5001')
+    server.start()
+    return server
+
 app = create_app(os.getenv("FLASK_ENV") or "test")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    grpc_server = serve_grpc()
+    try:
+        app.run(debug=True)  # Flask will run on the default port 5000
+    finally:
+        grpc_server.stop(0)  # Stop the gRPC server when the Flask app stops
