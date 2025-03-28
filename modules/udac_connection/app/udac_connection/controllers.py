90a64b4f87aa5a202db5560683b1ff2dd681f93c
Uwe Sülter <uwe.suelter@jtelekom.de>
2025-03-28 10:17:49 +0100

    Removed old version of get implementation.

--- modules/udac_connection/app/udac_connection/controllers.py
+++ modules/udac_connection/app/udac_connection/controllers.py
@@ -24,22 +24,6 @@ api = Namespace("udac_connection", description="Connections via geolocation.")  
 @api.param("distance", "Proximity to a given user in meters", _in="query")
 class ConnectionDataResource(Resource):
     @responds(schema=ConnectionSchema(many=True))
-    def post(self, person_id) -> ConnectionSchema:
-        start_date: datetime = datetime.strptime(
-            request.args["start_date"], DATE_FORMAT
-        )
-        end_date: datetime = datetime.strptime(request.args["end_date"], DATE_FORMAT)
-        distance: Optional[int] = request.args.get("distance", 5)
-
-        results = ConnectionService.find_contacts(
-            person_id=person_id,
-            start_date=start_date,
-            end_date=end_date,
-            meters=distance,
-        )
-        return results
-
-    @responds(schema=ConnectionSchema(many=True))
     def get(self, person_id) -> ConnectionSchema:
         try:
             start_date_str = request.args.get("start_date")
